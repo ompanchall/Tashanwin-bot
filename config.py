68e40a7e2d52799ee27294924bee91e52b70d316
Om PAnchal <pom54812@gmail.com>
2025-05-24 22:09:41 +0530

    Removed config.py after switching to environment variables

--- config.py
+++
@@ -1,5 +0,0 @@
-API_TOKEN = "7686945523:AAGaCc9tD8VY8O-dD487zczr8KkYoaJ_zLs"
-ADMIN_ID = 1681726379  # Replace with your Telegram numeric user ID
-REGISTER_LINK = "http://www.tashanwin.in/#/register?invitationCode=47134606717"
-CHANNEL_USERNAME = "https://t.me/+pXlGzrmlDNtmZDNl"
-ADMIN_CONTACT = "https://t.me/Soull_71"
